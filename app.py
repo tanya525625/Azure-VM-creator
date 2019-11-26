dc49ee4d0859e35dd0acfbb482b11a7a3526f93a
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2019-11-26 14:22:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request
-import subprocess
+#import subprocess
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return "test" # render_template('index.html')
 
-
+"""
 @app.route('/launch_vm/', methods=['POST'])
 def script():
     return str(subprocess.call("sh ./launch_yaml.sh", shell=True))
-
+"""
 
 if __name__ == "__main__":
     app.run()
