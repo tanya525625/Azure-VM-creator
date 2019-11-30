79bb322343c6ef3958a30bdf2e2e3aa42e2ab811
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-11-30 16:12:05 +0300

    VM creates

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "test" # render_template('index.html')
+    return render_template('index.html')
 
-"""
+
 @app.route('/launch_vm/', methods=['POST'])
 def script():
     return str(subprocess.call("sh ./launch_yaml.sh", shell=True))
-"""
+
 
 if __name__ == "__main__":
     app.run()
