67a3118d0753a49f901e7f40dfe47e78f5593d78
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-11-30 23:30:03 +0300

    Changed web

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def main():
 
 @app.route('/get_the_prediction/', methods=['POST'])
 def script():
-    name = request.form.get('name')
-    logging.debug(name)
+    # name = request.form.get('name')
+    # logging.debug(name)
     return str(subprocess.call("sh ./launch_yaml.sh", shell=True))
 
 
