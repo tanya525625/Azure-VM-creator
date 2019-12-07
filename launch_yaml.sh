edc46612ca56b24dccade21932170a84536a8b00
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2019-12-08 00:19:46 +0300

    Added credentials

--- launch_yaml.sh
+++ launch_yaml.sh
@@ -1,6 +1,1 @@
-mkdir ~/.azure
-touch ~/.azure/credentials
-echo -n "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
-pip3 install ansible[azure]
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 ansible-playbook launch_vm.yml
