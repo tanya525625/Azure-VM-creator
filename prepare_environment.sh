7b1fe74a78dc9bcc7710bcfd1497d3583c9ff433
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2019-12-07 23:54:01 +0300

    Added credentials

---
+++ prepare_environment.sh
@@ -0,0 +1,5 @@
+mkdir ~/.azure
+touch ~/.azure/credentials
+echo -n "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
+pip3 install ansible[azure]
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
