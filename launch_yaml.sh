dcf4179a5d74cdbe2658f7b5e9ad3a2ee3abc136
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-07 15:59:28 +0300

    Added credentials

--- launch_yaml.sh
+++ launch_yaml.sh
@@ -1,3 +1,5 @@
 mkdir ~/.azure
+touch ~/.azure/credentials
+echo -n "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 ansible-playbook launch_vm.yml
