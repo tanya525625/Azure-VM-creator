6b771c2b387fef09ef70c3168bd5275e5ecd98c1
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-16 01:20:28 +0300

    Debugging

--- mkdir.sh
+++ mkdir.sh
@@ -1,11 +1,16 @@
 DIR=~/.azure
+FILE=~/.azure/credentials
 
-if [ ! -d "$DIR" ]; then
-    # make dir if it doesn't exist
-    sh prepare_environment.sh
+if [ ! -f "$FILE" ]; then
+  if [ ! "$DIR" ]; then
+    mkdir ~/.azure
+  fi
+  # make dir if it doesn't exist
+  sh prepare_environment.sh
 fi
-pip3 install ansible[azure]
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
 pip3 install azure
 pip3 install azure-servicebus==0.21.1
+pip3 install ansible[azure]
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 apt install sshpass
--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,4 +1,3 @@
-mkdir ~/.azure
 touch ~/.azure/credentials
 echo -n "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
 
