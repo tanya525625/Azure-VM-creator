ee1ecc046b2fd043e9985ae63cf1af096753a422
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-16 01:01:46 +0300

    Debugging

--- mkdir.sh
+++ mkdir.sh
@@ -4,3 +4,8 @@ if [ ! -d "$DIR" ]; then
     # make dir if it doesn't exist
     sh prepare_environment.sh
 fi
+pip3 install ansible[azure]
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+pip3 install azure
+pip3 install azure-servicebus==0.21.1
+apt install sshpass
