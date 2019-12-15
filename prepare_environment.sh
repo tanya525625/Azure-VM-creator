c40327baf69f13c555341cdc1f8d7a321fde1d55
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-15 22:01:03 +0300

    Debugging

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -3,4 +3,6 @@ touch ~/.azure/credentials
 echo -n "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
 pip3 install ansible[azure]
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+pip3 install azure
+pip3 azure-servicebus==0.21.1
 apt-get install sshpass
