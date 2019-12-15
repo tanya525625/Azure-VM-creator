178a9b0e9206b1b0d5e40f30ef29b0b9c29dd450
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-16 00:46:45 +0300

    Debugging

--- launch_yaml.sh
+++ launch_yaml.sh
@@ -1,2 +1,7 @@
 sh mkdir.sh
+pip3 install ansible[azure]
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+pip3 install azure
+pip3 install azure-servicebus==0.21.1
+apt install sshpass
 ansible-playbook launch_vm.yml
