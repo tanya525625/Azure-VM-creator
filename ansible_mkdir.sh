7c5df613c7f3db67abb8573b364fca4dcf2de8fa
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-15 22:29:53 +0300

    Debugging

--- ansible_mkdir.sh
+++ ansible_mkdir.sh
@@ -1,7 +1,9 @@
-DIR=/etc/ansible
+DIR=/etc/ansible/
+FILE=/etc/ansible/hosts
 
-if [ ! -d "$DIR" ]; then
+if [ ! -f "$FILE" ]; then
     # make dir if it doesn't exist
-    sudo -n echo "VM ansible_host=$VM_IP ansible_ssh_user=$admin_username ansible_ssh_pass=$admin_password" > /etc/ansible/hosts
+    mkdir $DIR
+    sudo echo "VM ansible_host=$VM_IP ansible_ssh_user=$admin_username ansible_ssh_pass=$admin_password" > /etc/ansible/hosts
 fi
 ansible-playbook start_environment_preparation_for_VM.yml
