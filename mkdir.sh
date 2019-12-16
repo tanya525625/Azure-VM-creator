cf09e765da31d900be56ff4f0900c9cfab660834
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-16 03:01:47 +0300

    Debugging

--- mkdir.sh
+++ mkdir.sh
@@ -9,8 +9,8 @@ if [ ! -f "$FILE" ]; then
   sh prepare_environment.sh
 fi
 
-pip3 install azure
-pip3 install azure-servicebus==0.21.1
 pip3 install ansible[azure]
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+pip3 install azure
+pip3 install azure-servicebus==0.21.1
 apt install sshpass
