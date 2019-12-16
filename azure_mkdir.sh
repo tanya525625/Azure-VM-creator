a47f3b074295668b4ff2f436ebe492e9d1fdcb85
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2019-12-16 21:54:14 +0300

    Update azure_mkdir.sh

--- azure_mkdir.sh
+++ azure_mkdir.sh
@@ -10,9 +10,8 @@ if [ ! -f "$FILE" ]; then
   echo -n "[default]\nsubscription_id=$subscription_id\nclient_id=$client_id\nsecret=$secret\ntenant=$tenant">~/.azure/credentials
 fi
 
-# curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-# pip3 install azure
-
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+pip3 install azure
 apt install sshpass
 pip3 install ansible[azure]
 pip3 install azure-servicebus==0.21.0
