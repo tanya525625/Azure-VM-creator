272d7da464319a914b25b5759450e28dbfe351df
Tatiana_Zubakhina <Tatiana_Zubakhina@epam.com>
2019-12-16 19:30:42 +0300

    Debugging for parallel

--- ansible_mkdir.sh
+++ ansible_mkdir.sh
@@ -9,6 +9,7 @@ if [ ! -f "$FILE" ]; then
     echo "VM ansible_host=$VM_IP ansible_ssh_user=$admin_username ansible_ssh_pass=$admin_password" > /etc/ansible/hosts
 fi
 
-rm ~/.ssh/known_hosts
+
 export ANSIBLE_HOST_KEY_CHECKING=False
+rm ~/.ssh/known_hosts
 ansible-playbook start_environment_preparation_for_VM.yml
